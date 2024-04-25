4a688e7eb730e4ed4525ab9025168011d0b1d6ea
이영욱 <lyuk98@hotmail.com>
2024-04-25 12:38:01 +0900

    feat: 전역 테마 적용
    64b9988에서 추가한 테마를 적용합니다

--- frontend/connectus/App.tsx
+++ frontend/connectus/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigations/root/RootNavigator';
-import {Text, View} from 'react-native';
+import {defaultTheme} from '@/constants/themes';
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={defaultTheme}>
       <RootNavigator />
     </NavigationContainer>
   );
