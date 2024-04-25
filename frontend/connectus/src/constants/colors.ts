c3966148f1c49362d51fb368c9193f71e4d12bde
이영욱 <lyuk98@hotmail.com>
2024-04-25 12:31:04 +0900

    feat: 전역 색상 속성 추가

---
+++ frontend/connectus/src/constants/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  background: '#25242a',
+  buttonBackground: '#38363e',
+  primaryColorPink: '#f859fd',
+  primaryColorBlue: '#32b5ff',
+  secondaryColor: '#9b9b9b',
+  dividerColor: '#414141',
+  white: '#ffffff',
+};
+
+export default colors;
