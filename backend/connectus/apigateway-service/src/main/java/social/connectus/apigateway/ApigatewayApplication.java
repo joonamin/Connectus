975eed92ee19d25c5e03f3d10b87fac2da267075
박찬영 <albam6912@naver.com>
2024-04-26 16:49:22 +0900

    feat: apigateway 서비스 완료

--- backend/connectus/apigateway-service/src/main/java/social/connectus/apigateway/ApigatewayApplication.java
+++ backend/connectus/apigateway-service/src/main/java/social/connectus/apigateway/ApigatewayApplication.java
@@ -1,7 +1,12 @@
 package social.connectus.apigateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
 }
