668d132baebb476e023445b5df1bf466a9723f90
이흔오 <gms245@naver.com>
2024-05-01 01:10:38 +0900

    feat/bottom indicator

--- frontend/connectus/src/screens/map/MapWalkScreen.tsx
+++ frontend/connectus/src/screens/map/MapWalkScreen.tsx
@@ -1,8 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React, {useRef} from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import useUserLocation from '@/hooks/useUserLocation';
-import {useNavigation} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import colors from '@/constants/colors';
+import MainText from '@/components/text/MainText';
 
 export default function MapWalkScreen() {
   const mapRef = useRef<MapView | null>(null);
@@ -19,11 +22,24 @@ export default function MapWalkScreen() {
         showsMyLocationButton={true}
         region={{
           ...userLocation,
-          latitudeDelta: 0.012,
-          longitudeDelta: 0.011,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009,
         }}>
         {}
       </MapView>
+      <View style={styles.bottomIndicator}>
+        <View style={styles.indicatorContainer}>
+          <View style={styles.timeTextContainer}>
+            <Ionicons name="timer-outline" size={32} />
+            <MainText style={styles.timeText}>{'12:53'}</MainText>
+          </View>
+          <View style={styles.distanceTextContainer}>
+            <FontAwesome5 name={'walking'} size={28} />
+            <MainText style={styles.distanceText}>{'00:42KM'}</MainText>
+          </View>
+        </View>
+        <Ionicons style={styles.menuButton} name="menu" size={42} />
+      </View>
     </>
   );
 }
@@ -31,5 +47,45 @@ export default function MapWalkScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  bottomIndicator: {
+    width: Dimensions.get('screen').width,
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    position: 'absolute',
+    bottom: 0,
+  },
+  indicatorContainer: {
+    position: 'absolute',
+    left: 0,
+    paddingHorizontal: 15,
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  timeTextContainer: {
+    gap: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timeText: {
+    color: colors.background,
+  },
+  distanceTextContainer: {
+    gap: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  distanceText: {
+    color: colors.background,
+  },
+  menuButton: {
+    position: 'absolute',
+    right: 15,
   },
 });
