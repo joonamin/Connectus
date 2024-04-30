d8c9161dbb79dc7f32c5b99ce01abebca5d6df2c
이영욱 <lyuk98@hotmail.com>
2024-04-30 17:55:02 +0900

    feat: 마이페이지 초안 작성

---
+++ frontend/connectus/src/navigations/stack/MyStackNavigator.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import colors from '@/constants/colors';
+import MyHomeScreen from '@/screens/my/MyHomeScreen';
+
+export type MyStackParamList = {
+  MyHome: undefined;
+};
+
+const Stack = createStackNavigator<MyStackParamList>();
+
+function MyStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="MyHome"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          borderColor: colors.dividerColor,
+        },
+      }}>
+      <Stack.Screen
+        name="MyHome"
+        component={MyHomeScreen}
+        options={{
+          title: '마이페이지',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default MyStackNavigator;
--- frontend/connectus/src/screens/my/MyHomeScreen.tsx
+++ frontend/connectus/src/screens/my/MyHomeScreen.tsx
@@ -1,12 +1,66 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MainContainer from '@/components/containers/MainContainer';
+import CustomButton from '@/components/buttons/CustomButton';
+import ListItem from '@/components/containers/ListItem';
+import colors from '@/constants/colors';
+import MainText from '@/components/text/MainText';
+import IconListItem from '@/components/containers/IconListItem';
+import IconItemButton from '@/components/containers/IconItemButton';
 
 export default function MyHomeScreen() {
+  const styles = StyleSheet.create({
+    maincontainer: {
+      gap: 30,
+    },
+    listGroup: {
+      gap: 5,
+    },
+  });
+
   return (
-    <View>
-      <Text>MyHomeScreen</Text>
-    </View>
+    <MainContainer style={styles.maincontainer}>
+      <MainContainer style={styles.listGroup}>
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="MaterialCommunityIcons"
+          iconName="table-key"
+          text="이벤트 등록 권한 신청"
+        />
+      </MainContainer>
+      <MainContainer style={styles.listGroup}>
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="MaterialIcons"
+          iconName="person"
+          text="아바타 변경"
+        />
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="Ionicons"
+          iconName="trophy"
+          text="업적"
+        />
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="MaterialIcons"
+          iconName="list"
+          text="외출 기록"
+        />
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="MaterialIcons"
+          iconName="chat"
+          text="작성 댓글"
+        />
+        <IconItemButton
+          backgroundColor={colors.buttonBackground}
+          iconType="Ionicons"
+          iconName="heart"
+          text="좋아요 기록"
+        />
+      </MainContainer>
+    </MainContainer>
   );
 }
-
-const styles = StyleSheet.create({});
