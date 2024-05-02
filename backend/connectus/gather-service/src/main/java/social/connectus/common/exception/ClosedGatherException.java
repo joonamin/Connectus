7d4cf300af6e23343a8a74ea3f3dc4b71b1d92a7
박찬영 <albam6912@naver.com>
2024-05-02 11:51:21 +0900

    feat: ClosedGatherException 작성

--- backend/connectus/gather-service/src/main/java/social/connectus/common/exception/ClosedGatherException.java
+++ backend/connectus/gather-service/src/main/java/social/connectus/common/exception/ClosedGatherException.java
@@ -3,7 +3,7 @@ package social.connectus.common.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND)
+@ResponseStatus(value= HttpStatus.CONFLICT)
 public class ClosedGatherException extends Exception {
     public ClosedGatherException(String msg){
         super(msg);
