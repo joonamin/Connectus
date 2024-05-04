310235d76b2c3c7ae608d2c9d321a23f620a54c9
서현석 <gustjr580@naver.com>
2024-05-04 21:35:06 +0900

    walk service

---
+++ backend/connectus/walk-service/src/main/java/social/connectus/WalkApplication.java
@@ -0,0 +1,13 @@
+package social.connectus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WalkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalkApplication.class, args);
+    }
+}
