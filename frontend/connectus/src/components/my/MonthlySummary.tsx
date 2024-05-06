25cf2fd1e355b8d6895faaf7eec859cab67b3667
이영욱 <lyuk98@hotmail.com>
2024-05-06 16:05:32 +0900

    feat: 산책 기록 페이지 작성

---
+++ frontend/connectus/src/components/my/MonthlySummary.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import {StyleSheet, View, ViewProps} from 'react-native';
+import ListItem from '@/components/containers/ListItem';
+import MainText from '@/components/text/MainText';
+import LightText from '@/components/text/LightText';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '@/constants/colors';
+
+/**
+ * Badge를 생성 시 전달하는 인자를 지정합니다
+ */
+interface BadgeProps {
+  /**
+   * Badge 내 표시할 icon
+   */
+  icon: React.ReactNode;
+  /**
+   * 표시할 값
+   */
+  value: number | string;
+  /**
+   * 값에 대한 설명
+   */
+  label: string;
+}
+
+/**
+ * 월별 요약 지수 표시 시 하나의 수치를 생성합니다
+ *
+ * @returns Badge
+ */
+function Badge({icon, value, label}: BadgeProps) {
+  return (
+    <View style={styles.badge}>
+      {icon}
+      <MainText>{value}</MainText>
+      <LightText>{label}</LightText>
+    </View>
+  );
+}
+
+/**
+ * MonthlySummary를 생성 시 전달하는 인자를 지정합니다
+ */
+export interface MonthlySummaryProps extends ViewProps {
+  /**
+   * 산책 횟수
+   */
+  daysWalked: number;
+  /**
+   * 산책 거리
+   */
+  distance: number;
+  /**
+   * 산책 시간
+   */
+  timeSpent: number;
+}
+
+/**
+ * 월별 산책 요약 지수를 표시합니다
+ *
+ * @returns MonthlySummary
+ */
+export default function MonthlySummary({
+  daysWalked,
+  distance,
+  timeSpent,
+  ...props
+}: MonthlySummaryProps) {
+  const hours = Math.floor(timeSpent / 3600);
+  const minutes = Math.floor((timeSpent - hours * 3600) / 60);
+
+  return (
+    <ListItem {...props}>
+      <Badge
+        icon={<MaterialCommunityIcons name="calendar" style={styles.icon} />}
+        value={daysWalked}
+        label="산책 횟수"
+      />
+      <Badge
+        icon={<MaterialIcons name="location-pin" style={styles.icon} />}
+        value={distance.toFixed(2)}
+        label="외출 거리 (km)"
+      />
+      <Badge
+        icon={
+          <MaterialCommunityIcons name="timer-outline" style={styles.icon} />
+        }
+        value={hours + ':' + minutes}
+        label="외출 시간"
+      />
+    </ListItem>
+  );
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 36,
+    color: colors.white,
+  },
+  badge: {
+    flex: 1,
+    gap: 5,
+    alignItems: 'center',
+  },
+});
--- frontend/connectus/src/screens/my/MyWalkHistoryScreen.tsx
+++ frontend/connectus/src/screens/my/MyWalkHistoryScreen.tsx
@@ -1,12 +1,138 @@
+import MainContainer from '@/components/containers/MainContainer';
+import MonthlySummary from '@/components/my/MonthlySummary';
+import HeadingText from '@/components/text/HeadingText';
+import colors from '@/constants/colors';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, View} from 'react-native';
 
+/**
+ * 산책 기록을 표시합니다
+ *
+ * @returns MyWalkHistoryScreen
+ */
 export default function MyWalkHistoryScreen() {
-  return <></>;
+  // 시험용 데이터
+  const history = [
+    {
+      year: 2024,
+      month: 4,
+      summary: {
+        daysWalked: 14,
+        distance: 12.43,
+        timeSpent: 155520,
+      },
+      list: [
+        {
+          id: 5,
+        },
+        {
+          id: 4,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 1,
+        },
+      ],
+    },
+    {
+      year: 2024,
+      month: 3,
+      summary: {
+        daysWalked: 14,
+        distance: 12.43,
+        timeSpent: 155520,
+      },
+      list: [
+        {
+          id: 5,
+        },
+        {
+          id: 4,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 1,
+        },
+      ],
+    },
+  ];
+  // 기본 이미지
+  const defaultImage = require('@/assets/map.png');
+
+  return (
+    <ScrollView>
+      <View style={styles.page}>
+        {history.map(item => (
+          <View
+            key={
+              item.year + '-' + (item.month < 10)
+                ? '0' + item.month
+                : item.month
+            }
+            style={styles.month}>
+            <MainContainer style={styles.monthlyItem}>
+              <HeadingText>
+                {item.year}년 {item.month}월
+              </HeadingText>
+            </MainContainer>
+            <MainContainer style={styles.monthlyItem}>
+              <MonthlySummary
+                daysWalked={item.summary.daysWalked}
+                distance={item.summary.distance}
+                timeSpent={item.summary.timeSpent}
+              />
+            </MainContainer>
+            <View style={styles.historyGrid}>
+              {item.list.map(historyItem => (
+                <Image
+                  key={historyItem.id}
+                  style={styles.historyItem}
+                  source={defaultImage}
+                />
+              ))}
+            </View>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   page: {
+    paddingTop: 15,
     gap: 30,
+  },
+  month: {
+    gap: 15,
+  },
+  monthlyItem: {
+    paddingVertical: 0,
+  },
+  historyGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    gap: 0,
+  },
+  historyItem: {
+    flex: 1,
+    aspectRatio: '1 / 1',
+    flexBasis: '33.33333%',
+    flexGrow: 0,
+    flexShrink: 0,
+    resizeMode: 'cover',
+    borderWidth: 1,
+    borderColor: colors.background,
   },
 });
