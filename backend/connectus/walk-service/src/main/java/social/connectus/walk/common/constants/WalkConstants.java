2ef241547fe9ba145603601ceb12ec2f48a2d56f
박찬영 <albam6912@naver.com>
2024-05-07 09:39:55 +0900

    feat: walk common 작성

---
+++ backend/connectus/walk-service/src/main/java/social/connectus/walk/common/constants/WalkConstants.java
@@ -0,0 +1,11 @@
+package social.connectus.walk.common.constants;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class WalkConstants {
+    public static final String GATHER_NOT_FOUND = "Walk not found : ";
+    public static final String GATHERS_NOT_FOUND = "Walks not found : ";
+    public static final String REQUIRED_PARAMETER = "Required parameter ";
+    public static final String IS_NOT_PRESENT = " is not present";
+}
---
+++ backend/connectus/walk-service/src/main/java/social/connectus/walk/common/exception/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package social.connectus.walk.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> genericExceptionHandler(Exception ex, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
