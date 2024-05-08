0eb17ed18cbaa4de022f1b44bed148fa70ec476a
이영욱 <lyuk98@hotmail.com>
2024-05-08 17:56:03 +0900

    refactor: RouteMap 구조 변경

--- frontend/connectus/src/components/map/RouteMap.tsx
+++ frontend/connectus/src/components/map/RouteMap.tsx
@@ -1,14 +1,20 @@
 import colors from '@/constants/colors';
-import React, {useRef} from 'react';
+import React from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
-import MapView, {LatLng, PROVIDER_GOOGLE, Polyline} from 'react-native-maps';
+import MapView, {
+  LatLng,
+  MapViewProps,
+  PROVIDER_GOOGLE,
+  Polyline,
+} from 'react-native-maps';
 import StartMarker from '@/components/map/StartMarker';
 import FinishMarker from '@/components/map/FinishMarker';
+import {ViewProps} from 'react-native';
 
 /**
  * RouteMap을 생성할 시 전달할 인자를 지정합니다
  */
-export interface RouteMapProps {
+export interface RouteMapProps extends ViewProps {
   /**
    * 산책 경로
    */
@@ -20,17 +26,64 @@ export interface RouteMapProps {
  *
  * @returns RouteMap
  */
-export default function RouteMap({routes}: RouteMapProps) {
-  // 지도 ref 받아오기
-  const map = useRef<MapView>(null);
+export default class RouteMap extends React.Component {
+  /**
+   * 지도 객체
+   */
+  private map: MapView;
 
-  // 지도가 준비되었을 시 실행
-  const onMapReady = () => {
+  /**
+   * 지도에 표시할 경로 목록
+   */
+  protected routes: LatLng[];
+
+  /**
+   * View에 적용할 props
+   */
+  protected viewProps: ViewProps;
+
+  /**
+   * RouteMap을 생성합니다
+   *
+   * @param props RouterMap 생성 시 사용할 인자
+   */
+  constructor(props: RouteMapProps) {
+    super(props);
+
+    this.viewProps = props;
+    this.routes = props.routes;
+
+    const mapProps: MapViewProps = {
+      style: styles.map,
+      scrollEnabled: false,
+      zoomEnabled: false,
+      provider: PROVIDER_GOOGLE,
+      onMapReady: this.onMapReady.bind(this),
+      children: (
+        <>
+          <StartMarker coordinate={this.routes[0]} />
+          <FinishMarker coordinate={this.routes[this.routes.length - 1]} />
+          <Polyline
+            coordinates={this.routes}
+            strokeWidth={8}
+            strokeColor={colors.dividerColor}
+          />
+        </>
+      ),
+    };
+
+    this.map = new MapView(mapProps);
+  }
+
+  /**
+   * 지도 준비 완료 시 실행할 함수입니다
+   */
+  protected onMapReady() {
     // 지도를 적절한 위치에 위치
     if (Platform.OS === 'ios') {
-      map.current?.fitToElements();
+      this.map.fitToElements();
     } else {
-      map.current?.fitToCoordinates(routes, {
+      this.map.fitToCoordinates(this.routes, {
         animated: false,
         edgePadding: {
           top: 50,
@@ -40,29 +93,23 @@ export default function RouteMap({routes}: RouteMapProps) {
         },
       });
     }
-  };
+  }
 
-  return (
-    <View style={styles.mapContainer}>
-      <MapView
-        ref={map}
-        style={styles.map}
-        scrollEnabled={false}
-        zoomEnabled={false}
-        provider={PROVIDER_GOOGLE}
-        onMapReady={onMapReady}>
-        {/* 시작, 종료 마커 */}
-        <StartMarker coordinate={routes[0]} />
-        <FinishMarker coordinate={routes[routes.length - 1]} />
-        {/* 경로 line */}
-        <Polyline
-          coordinates={routes}
-          strokeWidth={8}
-          strokeColor={colors.dividerColor}
-        />
-      </MapView>
-    </View>
-  );
+  /**
+   * RouteMap을 render합니다
+   *
+   * @returns RouteMap
+   */
+  render() {
+    const {style: viewStyle, ...containerProps} = this.viewProps;
+    console.debug(this.map.props);
+
+    return (
+      <View style={[styles.mapContainer, viewStyle]} {...containerProps}>
+        {this.map.render()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
