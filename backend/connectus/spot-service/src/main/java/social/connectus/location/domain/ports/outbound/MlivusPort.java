5d14ac1815f28263819b988c966c1976bdac47f9
황진주 <jinjoo021@naver.com>
2024-05-08 17:39:36 +0900

    feat: Milvus 호출 기반 작성함
    Milvus 호출을 위한 Port 및 Adapter 정의함

---
+++ backend/connectus/spot-service/src/main/java/social/connectus/location/domain/ports/outbound/MlivusPort.java
@@ -0,0 +1,5 @@
+package social.connectus.location.domain.ports.outbound;
+
+public interface MlivusPort {
+    void MilvusTest();
+}
---
+++ backend/connectus/spot-service/src/main/java/social/connectus/location/infrastructure/milvus/MilvusAdapter.java
@@ -0,0 +1,19 @@
+package social.connectus.location.infrastructure.milvus;
+
+import io.milvus.client.MilvusServiceClient;
+import io.milvus.param.ConnectParam;
+import io.milvus.param.dml.InsertParam;
+import io.milvus.v2.common.DataType;
+import social.connectus.location.domain.ports.outbound.MlivusPort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MilvusAdapter implements MlivusPort {
+
+    @Override
+    public void MilvusTest() {
+
+    }
+}
