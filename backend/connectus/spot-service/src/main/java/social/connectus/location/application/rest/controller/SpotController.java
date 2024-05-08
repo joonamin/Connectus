e66a678065114af7a9d1968bf66449c0d4ad0b49
황진주 <jinjoo021@naver.com>
2024-05-09 05:25:31 +0900

    feat: Milvus 삽입/조회 테스트 완료함
    현재 데이터 구조에 맞도록 데이터 형식 생성 이후 생성 및 조회 처리를 진행함

--- backend/connectus/spot-service/src/main/java/social/connectus/location/application/rest/controller/SpotController.java
+++ backend/connectus/spot-service/src/main/java/social/connectus/location/application/rest/controller/SpotController.java
@@ -6,7 +6,10 @@ import io.milvus.param.ConnectParam;
 import io.milvus.param.dml.InsertParam;
 import io.milvus.v2.client.ConnectConfig;
 import io.milvus.v2.client.MilvusClientV2;
+import io.milvus.v2.service.collection.request.LoadCollectionReq;
 import io.milvus.v2.service.vector.request.InsertReq;
+import io.milvus.v2.service.vector.request.QueryReq;
+import io.milvus.v2.service.vector.response.QueryResp;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,47 +35,82 @@ public class SpotController {
 
     @GetMapping("/health_check")
     public String health_check(){
-        return "It's working on spot-service!";
-    }
+        // Milvus 연결 설정
         ConnectConfig connectConfig = ConnectConfig.builder()
                 .uri("https://milvus.connectus.social:19530")
                 .build();
 
         MilvusClientV2 client = new MilvusClientV2(connectConfig);
 
+
+        // 컬렉션 로드
+        String collectionName = "spot";
+
+        // 컬렉션 로드
+        System.out.println("컬렉션 로드");
+        LoadCollectionReq loadReq = LoadCollectionReq.builder()
+                .collectionName(collectionName)
+                .build();
+
+        client.loadCollection(loadReq);
+
+        // 데이터 생성
         Random ran = new Random();
-        List<Long> book_id_array = new ArrayList<>();
-        List<Long> word_count_array = new ArrayList<>();
-        List<List<Float>> book_intro_array = new ArrayList<>();
-        for (long i = 0L; i < 10; ++i) {
-            book_id_array.add(i);
-            word_count_array.add(i + 10000);
-            List<Float> vector = new ArrayList<>();
-            for (int k = 0; k < 2; ++k) {
-                vector.add(ran.nextFloat());
-            }
-            book_intro_array.add(vector);
-        }
+        JSONObject pingInfo = new JSONObject();
 
-        List<InsertParam.Field> fields = new ArrayList<>();
-        fields.add(new InsertParam.Field("book_id", book_id_array));
-        fields.add(new InsertParam.Field("word_count", word_count_array));
-        fields.add(new InsertParam.Field("book_intro", book_intro_array));
+        // 고유 ID
+        Long spot_id = Math.abs(ran.nextLong());
+        // 벡터 값 (경도, 위도)은 List<Float> 형태로 담아야함
+        Float longitude = ran.nextFloat(), latitude = ran.nextFloat();
+        List<Float> vectorList = new ArrayList<>();
+        vectorList.add(longitude);
+        vectorList.add(latitude);
+        // 그 외의 값 ping type, domain ID 생성
+        Long type = Math.abs(ran.nextLong()), domain_id = Math.abs(ran.nextLong());
 
-        JSONObject vector = new JSONObject();
-        vector.put("book_intro", book_intro_array.get(0));
+        // JSONObject 형태만 받기에 key/value 형태로 담아줌
+        // Mulivus Key는 Field 값과 일치 하도록 작성
+        pingInfo.put("spot_id", spot_id);
+        pingInfo.put("spot", vectorList);
+        pingInfo.put("type", type);
+        pingInfo.put("domain_id", domain_id);
 
+        System.out.println("생성 데이터");
+        System.out.println(pingInfo.toString());
+
+        // 삽입 요청
         InsertReq insertReq = InsertReq.builder()
-                .collectionName("book")
-                .data(Collections.singletonList(vector))
+                .collectionName(collectionName)
+                .data(Collections.singletonList(pingInfo))
                 .build();
         client.insert(insertReq);
 
-        client.insert(InsertReq.builder()
-                .collectionName("book")
-                .data(Collections.singletonList(vector))
-                .build()
-        );
+        System.out.println("삽입 완료!");
+
+        // 조회 요청
+        String filter = "spot_id != " + -1;
+
+        // 조회 요청 생성
+        QueryReq queryReq = QueryReq.builder()
+                .collectionName(collectionName)
+                .filter(filter)
+//                .outputFields(Arrays.asList("spot_id", "spot", "type", "domain_id")) // 조회할 필드 목록
+                .limit(10) // 원하는 결과의 최대 개수
+                .build();
+
+        // 쿼리 수행
+        QueryResp results = client.query(queryReq);
+
+        System.out.println("조회 결과:");
+        for (int i = 0; i < results.getQueryResults().size(); i++) {
+            QueryResp.QueryResult queryResult = results.getQueryResults().get(i);
+            Map<String, Object> row = queryResult.getEntity();
+            System.out.println("Row " + i + ": " + row);
+        }
+
+        return "It's working on spot-service!";
+    }
+
 
     @PostMapping("/findNearby")
     public String findNearbyElement(FindNearbyElementRequest request) {
