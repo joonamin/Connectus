e7992b39cd4db9e0e18bcc1d658af2f9d4acea68
황진주 <jinjoo021@naver.com>
2024-05-09 12:29:07 +0900

    feat: 로직 변화에 따른 Ping 정의값 변환
     - 모든 위치 데이터 관리가 벡터DB로 이루어 지는 것으로 결정
     - 그에 따라 작성일 데이터가 추가됨

--- backend/connectus/spot-service/src/main/java/social/connectus/location/common/type/Ping.java
+++ backend/connectus/spot-service/src/main/java/social/connectus/location/common/type/Ping.java
@@ -3,6 +3,8 @@ package social.connectus.location.common.type;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class Ping {
@@ -11,5 +13,6 @@ public class Ping {
     private double latitude;
     private PingType type;
     private long domainId;
-    private String contents;
+    private LocalDateTime cretedAt;
+    private LocalDateTime updatedAt;
 }
