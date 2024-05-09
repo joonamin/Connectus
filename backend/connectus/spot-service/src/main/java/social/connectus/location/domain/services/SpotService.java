dbb8ae0c64e304bfb9d2583b27cc9b9885fa20fb
황진주 <jinjoo021@naver.com>
2024-05-10 00:56:42 +0900

    feat: 기본적인 Spot 전체 조회 구현함
    - 기본 커스텀 쿼리를 이용하여 전체 조회를 구현함
    - 이후 특정 거리 범위로 수정 예정

--- backend/connectus/spot-service/src/main/java/social/connectus/location/domain/services/SpotService.java
+++ backend/connectus/spot-service/src/main/java/social/connectus/location/domain/services/SpotService.java
@@ -12,47 +12,46 @@ import social.connectus.location.domain.ports.inbound.SpotUseCase;
 import social.connectus.location.domain.ports.outbound.MilvusPort;
 import social.connectus.location.domain.ports.outbound.SpotPort;
 
+import java.lang.reflect.Type;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Transactional
-@Component
+@Service
 public class SpotService implements SpotUseCase {
 
+//    private final SpotPort spotPort;
     private final MilvusPort milvusPort;
 
     @Override
     public FindNearbyElementResponse findNearbyElement(FindNearbyElementCommand command) {
         QueryResp resp = milvusPort.select(command.getLongitude(), command.getLatitude());
+        System.out.println(resp);
+
         List<QueryResp.QueryResult> results = resp.getQueryResults();
 
         ArrayList<Ping> nearby = new ArrayList<>();
         for (QueryResp.QueryResult queryResult : results) {
             Map<String, Object> row = queryResult.getEntity();
-            /*
-             * {
-             *  domain_id=4305556974050930190,
-             *  updated_at=2024-05-09T11:34:22.928575400,
-             *  spot_id=449616917989181707,
-             *  spot=[0.15342987, 0.8820968],
-             *  created_at=2024-05-09T11:34:22.928575400,
-             *  type=2854591712224326841}
-             * */
-            ArrayList<Double> coordinate = (ArrayList<Double>) row.get("spot");
+
+            List<Float> coordinate = (List<Float>) row.get("spot");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS");
+
             Ping ping = Ping.builder()
                     .id((Long) row.get("spot_id"))
                     .domainId((Long) row.get("domain_id"))
                     .longitude(coordinate.get(0))
                     .latitude(coordinate.get(1))
                     .type(PingType.valueOf((String) row.get("type")))
-                    .cretedAt((LocalDateTime) row.get("created_at"))
-                    .updatedAt((LocalDateTime) row.get("updated_at"))
+                    .cretedAt(LocalDateTime.parse((String) row.get("created_at"), formatter))
+                    .updatedAt(LocalDateTime.parse((String) row.get("updated_at"), formatter))
                     .build();
             nearby.add(ping);
-//            System.out.println("Row " + i + ": " + row);
         }
         return FindNearbyElementResponse.builder()
                 .nearby(nearby)
