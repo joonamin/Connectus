ff39bc40d46f1594b46ec9bf5ff904e00d8ffff2
서현석 <gustjr580@naver.com>
2024-05-10 00:40:19 +0900

    post to like

--- backend/connectus/post-service/src/main/java/social/connectus/infrastructure/feignClient/LikesServiceClient.java
+++ backend/connectus/post-service/src/main/java/social/connectus/infrastructure/feignClient/LikesServiceClient.java
@@ -3,11 +3,12 @@ package social.connectus.infrastructure.feignClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "likes-service")
 public interface LikesServiceClient {
 	@GetMapping("/likes/{postId}/like-count")
-	int getLikeCount(@PathVariable Long postId, String type);
+	int getLikeCount(@PathVariable Long postId, @RequestParam String typeString);
 
 	@GetMapping("/likes/{postId}/is-like")
 	boolean isLike(@PathVariable Long postId);
