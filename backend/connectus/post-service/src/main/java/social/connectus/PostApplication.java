a0919df8a8f9ff69baad06274b8c9d0bf855898d
서현석 <gustjr580@naver.com>
2024-05-09 22:28:24 +0900

    fix conflict

--- backend/connectus/post-service/src/main/java/social/connectus/PostApplication.java
+++ backend/connectus/post-service/src/main/java/social/connectus/PostApplication.java
@@ -2,11 +2,11 @@ package social.connectus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 @EnableFeignClients
 public class PostApplication {
 	public static void main(String[] args) {
--- backend/connectus/post-service/src/main/java/social/connectus/infrastructure/databases/mariadb/repository/FeedRepository.java
+++ backend/connectus/post-service/src/main/java/social/connectus/infrastructure/databases/mariadb/repository/FeedRepository.java
@@ -1,8 +1,10 @@
 package social.connectus.infrastructure.databases.mariadb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import social.connectus.domain.model.RDBMS.Feed;
 
+@Repository
 public interface FeedRepository extends JpaRepository<Feed,Long>, FeedRepositoryCustom {
 }
