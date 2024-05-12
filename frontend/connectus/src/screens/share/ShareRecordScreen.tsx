1631b3aa7431ed2a396e1c18fdd4ece26fbb08f3
이영욱 <lyuk98@hotmail.com>
2024-05-13 02:22:24 +0900

    feat: 공유할 기록 선택 화면 작성

--- frontend/connectus/src/screens/share/ShareRecordScreen.tsx
+++ frontend/connectus/src/screens/share/ShareRecordScreen.tsx
@@ -1,16 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView} from 'react-native';
 import React from 'react';
-import MainContainer from '@/components/containers/MainContainer';
+import WalkHistory, {WalkHistoryProps} from '@/components/my/WalkHistory';
 
 /**
  * 유저의 지난 산책 기록들을 선택해서 공유할 수 있는 스크린입니다.
  */
 export default function ShareRecordScreen() {
+  const onShareItemSelected: WalkHistoryProps['onWalkSelected'] = () => {
+    // 공유할 기록 선택 시 관련 작업 수행
+  };
+
   return (
-    <MainContainer>
-      <Text>ShareRecordScreen</Text>
-    </MainContainer>
+    <ScrollView>
+      <WalkHistory onWalkSelected={onShareItemSelected} showSummary={false} />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({});
