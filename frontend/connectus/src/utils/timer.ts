1cacd7ffceb255910d9fc47da72b211d39aaa230
이흔오 <gms245@naver.com>
2024-05-14 01:35:50 +0900

    feat : 모여라 스크린에서 받아올 종료시간으로 인한 계산 함수를 작성했습니다

--- frontend/connectus/src/utils/timer.ts
+++ frontend/connectus/src/utils/timer.ts
@@ -1,3 +1,8 @@
+/**
+ * 하단 인디케이터에서 사용할 몇초동안 걸었는지를 string으로 표시해줄 함수
+ * @param seconds
+ * @returns
+ */
 function convertSecondsToTime(seconds: number): {
   hours: number;
   minutes: number;
@@ -13,10 +18,29 @@ function padSingleDigit(num: number): string {
   return num < 10 ? `0${num}` : `${num}`;
 }
 
+/**
+ * 모여라 타이머에 넣어줄 date값을위해 두 string형태의 date를 가져와 빼기연산을 진행합니다.
+ * @param time1
+ * @param time2
+ * @returns 시간차이를 초로 계산해서 넘겨줍니다
+ */
+function getMinuteDifference(time1: string, time2: string): number {
+  // 시간 문자열을 Date 객체로 변환
+  const date1 = new Date(time1);
+  const date2 = new Date(time2);
+  console.log('data1 : ', date1);
+  console.log('data2 : ', date2);
+  const differenceInMs = date2.getTime() - date1.getTime();
+
+  const differenceInSeconds = Math.round(Math.abs(differenceInMs) / 1000);
+
+  return differenceInSeconds;
+}
+
 function formatTime(hours: number, minutes: number, seconds: number): string {
   return `${padSingleDigit(hours)}:${padSingleDigit(minutes)}:${padSingleDigit(
     seconds,
   )}`;
 }
 
-export {convertSecondsToTime, formatTime};
+export {convertSecondsToTime, formatTime, getMinuteDifference};
