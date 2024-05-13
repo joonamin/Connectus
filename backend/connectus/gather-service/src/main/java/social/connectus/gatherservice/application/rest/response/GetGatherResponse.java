c0f4b27c651d7c99f72cde026c720bf3d047c861
박찬영 <albam6912@naver.com>
2024-05-13 10:37:33 +0900

    fix: 모여라 상세 조회 시 createdAt, updateAt 반환 추가

--- backend/connectus/gather-service/src/main/java/social/connectus/gatherservice/application/rest/response/GetGatherResponse.java
+++ backend/connectus/gather-service/src/main/java/social/connectus/gatherservice/application/rest/response/GetGatherResponse.java
@@ -2,6 +2,7 @@ package social.connectus.gatherservice.application.rest.response;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -19,4 +20,5 @@ public class GetGatherResponse {
     private Set<Long> candidateList;
     private Set<Long> joinerList;
     private String endTime;
+    private LocalDateTime updatedAt;
 }
