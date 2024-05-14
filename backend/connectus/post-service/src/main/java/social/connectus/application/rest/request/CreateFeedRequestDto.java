6e19ac5bd9e27f82d8d75abb2343e18aa5651db4
서현석 <gustjr580@naver.com>
2024-05-14 15:09:06 +0900

    fix : createPost

--- backend/connectus/post-service/src/main/java/social/connectus/application/rest/request/CreateFeedRequestDto.java
+++ backend/connectus/post-service/src/main/java/social/connectus/application/rest/request/CreateFeedRequestDto.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class CreateFeedRequestDto {
 	private Long walkId;
 	private List<PostRequestDto> postList;
