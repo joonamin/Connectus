b6084f6901b427481f2b88452a6649e11a09eefa
이흔오 <gms245@naver.com>
2024-05-14 16:30:45 +0900

    feat : 전역적으로 관리할 query key를 추가했습니다

--- frontend/connectus/src/constants/index.ts
+++ frontend/connectus/src/constants/index.ts
@@ -1,2 +1,3 @@
 // constants 폴더의 파일을 편하게 export 하기위한 파일입니다.
 export * from './fonts';
+export * from './keys';
---
+++ frontend/connectus/src/constants/keys.ts
@@ -0,0 +1,10 @@
+/**
+ * react query key를 관리할 파일입니다.
+ */
+
+const queryKeys = {
+  GET_GATHER: 'getGather',
+  GET_FEED_LIST: 'getFeedList',
+} as const;
+
+export {queryKeys};
