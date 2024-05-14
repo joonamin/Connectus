37e5f2e4058ef351a2fa94818c4f5760f7bdd6d7
이흔오 <gms245@naver.com>
2024-05-14 16:30:16 +0900

    feat : app 안의 react query key를 관리할 컨테이너를 생성했습니다

--- frontend/connectus/App.tsx
+++ frontend/connectus/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigations/root/RootNavigator';
 import {defaultTheme} from '@/constants/themes';
+import {QueryClientProvider} from '@tanstack/react-query';
+import queryClient from '@/api/queryClient';
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer theme={defaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer theme={defaultTheme}>
+        <RootNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
