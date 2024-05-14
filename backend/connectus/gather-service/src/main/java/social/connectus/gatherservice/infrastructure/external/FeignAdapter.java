23ae9178063745ff3f66723eeec2922681bea22a
박찬영 <albam6912@naver.com>
2024-05-14 11:28:57 +0900

    feat: 모여라쪽에 spot서비스와 연결하는 feignClient 작성

--- backend/connectus/gather-service/src/main/java/social/connectus/gatherservice/infrastructure/external/FeignAdapter.java
+++ backend/connectus/gather-service/src/main/java/social/connectus/gatherservice/infrastructure/external/FeignAdapter.java
@@ -1,16 +1,21 @@
 package social.connectus.gatherservice.infrastructure.external;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import social.connectus.gatherservice.application.rest.request.CreateSpotRequest;
 import social.connectus.gatherservice.application.rest.request.UpdateSpotRequest;
 
+@Component
+@RequiredArgsConstructor
 public class FeignAdapter implements FeignPort{
+    private final SpotClient spotClient;
 
     // TODO: 위치 서비스와 연결하여 특정 요소 추가 메서드 실행.
     public long CreateSpot(CreateSpotRequest request){
-        return 0;
+        return spotClient.CreateSpot(request);
     }
     @Override
     public void UpdateSpot(UpdateSpotRequest request) {
-        return;
+        spotClient.UpdateSpot(request);
     }
 }
