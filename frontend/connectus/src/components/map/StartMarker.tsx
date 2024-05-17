129b438abcfffd99d3cfb5763c4445bda733514f
이영욱 <lyuk98@hotmail.com>
2024-05-18 05:38:37 +0900

    fix: RouteMap 마커 오류 수정
    StartMarker와 FinishMarker가 처음 로드 시 보이지 않는 문제를 수정합니다
    
    두 마커에 사용되는 이미지 파일의 크기가 조정됩니다
    
    https://github.com/react-native-maps/react-native-maps/issues/924

--- frontend/connectus/src/components/map/StartMarker.tsx
+++ frontend/connectus/src/components/map/StartMarker.tsx
@@ -9,27 +9,11 @@ import {MapMarkerProps, Marker} from 'react-native-maps';
  */
 export default function StartMarker({...props}: MapMarkerProps) {
   return (
-    <Marker identifier="start" tracksViewChanges={false} {...props}>
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require('@/assets/markers/ping_start.png')}
-        />
-      </View>
-    </Marker>
+    <Marker
+      identifier="start"
+      tracksViewChanges={false}
+      image={require('@/assets/markers/ping_start.png')}
+      {...props}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: 70,
-    aspectRatio: '207 / 250',
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-  },
-});
