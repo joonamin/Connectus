ad529ee1ef30dc3b0d70ca6774343d86a45ee94d
이영욱 <lyuk98@hotmail.com>
2024-05-17 15:46:42 +0900

    feat: StartMarker, FinishMarker 이미지 변경

--- frontend/connectus/src/components/map/FinishMarker.tsx
+++ frontend/connectus/src/components/map/FinishMarker.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
 import {MapMarkerProps, Marker} from 'react-native-maps';
 
 /**
@@ -7,5 +8,28 @@ import {MapMarkerProps, Marker} from 'react-native-maps';
  * @returns FinishMarker
  */
 export default function FinishMarker({...props}: MapMarkerProps) {
-  return <Marker identifier="finish" {...props} />;
+  return (
+    <Marker identifier="finish" tracksViewChanges={false} {...props}>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require('@/assets/markers/ping_finish.png')}
+        />
+      </View>
+    </Marker>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 70,
+    aspectRatio: '207 / 250',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
