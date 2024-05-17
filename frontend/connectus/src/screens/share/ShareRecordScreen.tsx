0a47da731fe404f44774488b71ec9441e2dd1dc0
이영욱 <lyuk98@hotmail.com>
2024-05-17 12:22:50 +0900

    feat: 경로 공유 기능 작성
    경로 공유를 요청하는 기능을 작성합니다

--- frontend/connectus/src/screens/share/ShareRecordScreen.tsx
+++ frontend/connectus/src/screens/share/ShareRecordScreen.tsx
@@ -13,38 +13,31 @@ import SecondaryButton, {
 } from '@/components/buttons/SecondaryButton';
 import LightText from '@/components/text/LightText';
 import Dialog from '@/components/containers/Dialog';
+import {useMutation} from '@tanstack/react-query';
+import useAuthStore from '@/store/useAuthStore';
+import {shareRoute} from '@/api/walk';
+import queryClient from '@/api/queryClient';
+import {queryKeys} from '@/constants';
 
 /**
  * 유저의 지난 산책 기록들을 선택해서 공유할 수 있는 스크린입니다.
  */
 export default function ShareRecordScreen() {
+  // 사용자 정보 불러오기
+  const {user} = useAuthStore();
+
   // 선택 시 설정 될 산책 ID
   const [walkId, setWalkId] = useState<number | undefined>();
 
   // modal 관련 설정
   const [shareDialogVisibility, setShareDialogVisibility] =
     useState<boolean>(false);
-  const [dialogMessage, setDialogMessage] = useState<string | undefined>();
   const [closeTimout, setCloseTimeout] = useState<
     ReturnType<typeof setTimeout> | undefined
   >();
   const closeShareDialog = () => {
     setShareDialogVisibility(false);
     clearTimeout(closeTimout);
-    setDialogMessage(undefined);
-  };
-
-  // Axios 요청을 흉내내기 위한 시험 코드
-  const testShare = () => {
-    return new Promise<undefined>((resolve, reject) => {
-      setTimeout(() => {
-        if (walkId === 6) {
-          reject('I do not like number six');
-        } else {
-          resolve(undefined);
-        }
-      }, 1500);
-    });
   };
 
   const onShareItemSelected: WalkHistoryProps['onWalkSelected'] = (
@@ -55,28 +48,32 @@ export default function ShareRecordScreen() {
     setShareDialogVisibility(true);
   };
 
+  // 공유 mutation 설정
+  const mutation = useMutation({
+    mutationFn: (shareId: number) =>
+      shareRoute(shareId, user?.userId as number),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: [queryKeys.GET_ROUTE_LIST]});
+    },
+    onSettled: () => {
+      // 메세지 표시 3초 후 modal 닫기
+      setCloseTimeout(
+        setTimeout(() => {
+          closeShareDialog();
+          mutation.reset();
+        }, 3000),
+      );
+    },
+  });
+
   // 경로 공유 시 실행
   const onShare = () => {
-    console.debug(walkId);
-
-    setDialogMessage('경로 공유 중');
+    if (typeof walkId === 'undefined') {
+      return;
+    }
 
     // 경로 공유 작업 진행
-    testShare()
-      .then(() => {
-        setDialogMessage('공유가 완료되었습니다');
-      })
-      .catch(() => {
-        setDialogMessage('오류가 발생했습니다');
-      })
-      .finally(() => {
-        // 메세지 표시 3초 후 modal 닫기
-        setCloseTimeout(
-          setTimeout(() => {
-            closeShareDialog();
-          }, 3000),
-        );
-      });
+    mutation.mutate(walkId);
   };
 
   return (
@@ -89,7 +86,7 @@ export default function ShareRecordScreen() {
         transparent={true}
         animationType="fade"
         onRequestClose={closeShareDialog}>
-        {typeof dialogMessage === 'undefined' ? (
+        {mutation.isIdle ? (
           <>
             <LightText>해당 경로를 공유하시겠어요?</LightText>
             <View style={styles.buttonRow}>
@@ -102,7 +99,11 @@ export default function ShareRecordScreen() {
             </View>
           </>
         ) : (
-          <LightText>{dialogMessage}</LightText>
+          <LightText>
+            {mutation.isPending ? '경로 공유 중' : undefined}
+            {mutation.isError ? '오류가 발생했습니다' : undefined}
+            {mutation.isSuccess ? '공유가 완료되었습니다' : undefined}
+          </LightText>
         )}
       </Dialog>
     </ScrollView>
