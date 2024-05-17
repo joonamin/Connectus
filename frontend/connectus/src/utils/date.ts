34437bee82c9f979f4f57948069675c3b3b5b079
이흔오 <gms245@naver.com>
2024-05-17 09:51:02 +0900

    feat : string형식의 데이트 타입을 받아와서 시, 분까지 반환하는 함수 작성

--- frontend/connectus/src/utils/date.ts
+++ frontend/connectus/src/utils/date.ts
@@ -36,6 +36,16 @@ function dateToString(date: Date) {
   return `${year}-${month}-${day}`;
 }
 
+function dateTimeToString(dateString: string) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
+
 function getCurrentTimeAsStringWithMS() {
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, '0');
@@ -45,4 +55,9 @@ function getCurrentTimeAsStringWithMS() {
   return `${hours}${minutes}${seconds}${milliseconds}`;
 }
 
-export {getSubmitDate, dateToString, getCurrentTimeAsStringWithMS};
+export {
+  getSubmitDate,
+  dateToString,
+  dateTimeToString,
+  getCurrentTimeAsStringWithMS,
+};
