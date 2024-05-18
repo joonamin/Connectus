583a389d6b8627b9c973b5de303842cd0f6f4c94
이영욱 <lyuk98@hotmail.com>
2024-05-19 07:40:28 +0900

    refactor: ProfileOverview 내 styles 이동

--- frontend/connectus/src/components/my/ProfileOverview.tsx
+++ frontend/connectus/src/components/my/ProfileOverview.tsx
@@ -25,40 +25,6 @@ export default function ProfileOverview({
   profileImage,
   ...props
 }: ProfileOverviewProps) {
-  const avatarStyles = StyleSheet.create({
-    avatar: {
-      aspectRatio: '1 / 1',
-      height: 96,
-      borderRadius: 180,
-      backgroundColor: colors.buttonBackground,
-      alignSelf: 'center',
-      borderColor: colors.white,
-      borderWidth: 2,
-      zIndex: 1,
-    },
-  });
-  const styles = StyleSheet.create({
-    container: {
-      marginBottom: -(avatarStyles.avatar['height'] / 2)
-    },
-    overview: {
-      top: -(avatarStyles.avatar['height'] / 2),
-      paddingTop: avatarStyles.avatar['height'] / 2 + 15,
-      backgroundColor: colors.buttonBackground,
-      borderRadius: 15,
-      gap: 15,
-    },
-    username: {
-      alignSelf: 'center',
-    },
-    badges: {
-      flexDirection: 'row',
-    },
-    badge: {
-      flex: 1,
-    }
-  });
-
   // 프로필 이미지가 없을 시 기본 이미지 사용
   if (typeof profileImage === 'undefined') {
     profileImage = require('@/assets/default-profile.png');
@@ -73,11 +39,53 @@ export default function ProfileOverview({
       <MainContainer style={styles.overview}>
         <HeadingText style={styles.username}>한방 옻닭</HeadingText>
         <View style={styles.badges}>
-          <StatisticsBadge style={styles.badge} label='보유 포인트' value={320} />
-          <StatisticsBadge style={styles.badge} label='작성 방명록' value={32} />
-          <StatisticsBadge style={styles.badge} label='달성 업적' value={12} />
+          <StatisticsBadge
+            style={styles.badge}
+            label="보유 포인트"
+            value={320}
+          />
+          <StatisticsBadge
+            style={styles.badge}
+            label="작성 방명록"
+            value={32}
+          />
+          <StatisticsBadge style={styles.badge} label="달성 업적" value={12} />
         </View>
       </MainContainer>
     </View>
   );
 }
+
+const avatarStyles = StyleSheet.create({
+  avatar: {
+    aspectRatio: '1 / 1',
+    height: 96,
+    borderRadius: 180,
+    backgroundColor: colors.buttonBackground,
+    alignSelf: 'center',
+    borderColor: colors.white,
+    borderWidth: 2,
+    zIndex: 1,
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: -(avatarStyles.avatar['height'] / 2),
+  },
+  overview: {
+    top: -(avatarStyles.avatar['height'] / 2),
+    paddingTop: avatarStyles.avatar['height'] / 2 + 15,
+    backgroundColor: colors.buttonBackground,
+    borderRadius: 15,
+    gap: 15,
+  },
+  username: {
+    alignSelf: 'center',
+  },
+  badges: {
+    flexDirection: 'row',
+  },
+  badge: {
+    flex: 1,
+  },
+});
